5c71036adc44a8962c183a52aaf769e712ec2277
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-08-25 09:10:43 +0530

    yay

---
+++ lab3/pr3.js
@@ -0,0 +1,13 @@
+/*import http from "http";
+const server = http.createServer((req,res)=>{
+    if(req.url=="/"){
+        res.end("<h1> home page </h1>");
+    }
+    else if(req.url=="/about"){
+        res.end("<h1> about page </h1>");
+    }
+    else if (req.url=="/product"){
+        res.end('<h1> mobile phone </h1> <h2> laptop </h2> <h3> tablet </h3><a href="#"> g </a>');
+                };
+
+)}; */
