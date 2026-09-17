3c8617f3d4b495ab6cb3e29f6ec38c025d6279c4
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-09-17 13:59:05 +0530

    basic routing with express server

--- lab5/app.js
+++ lab5/app.js
@@ -1,8 +1,30 @@
+
 import express from 'express';
 
 const app = express();
 
-app.get("/about", (req,res) => {
-    res.send("<h1> hello yay")
+app.get("/about", (req, res) => {
+    res.send("<h1> hello yay</h1>");
 });
-app.listen(3000, ()=> console.log ("server is running"));
+
+app.get('/about1', (req, res) => {
+    res.send("WE ARE");
+});
+
+app.put('/user/update/1', (req,res)=>{
+    res.send({msg: 'user update'})
+})
+
+app.post('/login', (req,res)=>{
+    res.send({msg: 'user login'})
+})
+
+app.delete('/users/1', (req,res) => {
+    res.send({msg:'remove user 1'})
+})
+
+app.use((req, res) => {
+    res.status(404).send("not found");
+});
+
+app.listen(3333, () => console.log("server is running at 3333"));
